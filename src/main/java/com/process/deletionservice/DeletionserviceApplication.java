49e519253e36e645d5b09a36af322bfe03015bdb
Tushar Bhatia <tusharbhatia1614@gmail.com>
2025-04-03 19:34:06 +0530

    This commit sets up teh basic structure of deletion service

---
+++ src/main/java/com/process/deletionservice/DeletionserviceApplication.java
@@ -0,0 +1,13 @@
+package com.process.deletionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeletionserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeletionserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/process/deletionservice/DeletionserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.process.deletionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeletionserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
